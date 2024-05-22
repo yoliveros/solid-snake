66b8be1421ce362df0e5709e201cad3b36863636
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-05-22 18:17:09 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <p>
+                Hola World
+            </p>
+        </>
+    )
+}
+
+export default App
